0dd313c6a93588cc9c3ef2b01045981b13963da9
hifi <hifijhu@gmail.com>
2026-01-14 20:34:00 +0800

    convert apis into list

--- api_test.py
+++ api_test.py
@@ -10,6 +10,7 @@ env:
         Python: 3.10
         Foolbox: 3.3.4
         DCU: Z100L * 7
+        Model: Resnet-18
 A simple example that demonstrates how to run a single attack against
 a PyTorch ResNet-18 model for different epsilons and how to then report
 the robust accuracy.
@@ -17,8 +18,10 @@ the robust accuracy.
 import torchvision.models as models
 import eagerpy as ep
 from foolbox import PyTorchModel, accuracy, samples
+import foolbox.attacks as atks
 from foolbox.attacks import LinfPGD
 
+import inspect
 
 def main() -> None:
     # instantiate a model (could also be a TensorFlow or JAX model)
@@ -34,48 +37,106 @@ def main() -> None:
     print(f"clean accuracy:  {clean_acc * 100:.1f} %")
 
     # apply the attack
-    attack = LinfPGD()
-    epsilons = [
-        0.0,
-        0.0002,
-        0.0005,
-        0.0008,
-        0.001,
-        0.0015,
-        0.002,
-        0.003,
-        0.01,
-        0.1,
-        0.3,
-        0.5,
-        1.0,
+    attacks = [
+        atks.L2ContrastReductionAttack,
+        atks.VirtualAdversarialAttack,
+        atks.DDNAttack,
+        atks.L2ProjectedGradientDescentAttack,
+        atks.LinfProjectedGradientDescentAttack,
+        atks.L2BasicIterativeAttack,
+        atks.LinfBasicIterativeAttack,
+        atks.L2FastGradientAttack,
+        atks.LinfFastGradientAttack,
+
+        atks.L2AdditiveGaussianNoiseAttack,
+        atks.L2AdditiveUniformNoiseAttack,
+        atks.L2ClippingAwareAdditiveGaussianNoiseAttack,
+        atks.L2ClippingAwareAdditiveUniformNoiseAttack,
+        atks.LinfAdditiveUniformNoiseAttack,
+        atks.L2RepeatedAdditiveGaussianNoiseAttack,
+        atks.L2RepeatedAdditiveUniformNoiseAttack,
+        atks.L2ClippingAwareRepeatedAdditiveGaussianNoiseAttack,
+        atks.L2ClippingAwareRepeatedAdditiveUniformNoiseAttack,
+        atks.LinfRepeatedAdditiveUniformNoiseAttack,
+        atks.InversionAttack,
+        atks.BinarySearchContrastReductionAttack,
+        atks.LinearSearchContrastReductionAttack,
+
+        atks.HopSkipJumpAttack,
+
+        atks.L2CarliniWagnerAttack,
+        atks.NewtonFoolAttack,
+        atks.EADAttack,
+        atks.GaussianBlurAttack,
+        atks.L2DeepFoolAttack,
+        atks.LinfDeepFoolAttack,
+        atks.SaltAndPepperNoiseAttack,
+        atks.LinearSearchBlendedUniformNoiseAttack,
+        atks.BinarizationRefinementAttack,
+        atks.DatasetAttack,
+        atks.BoundaryAttack,
+        atks.L0BrendelBethgeAttack,
+        atks.L1BrendelBethgeAttack,
+        atks.L2BrendelBethgeAttack,
+        atks.LinfinityBrendelBethgeAttack,
+        atks.L0FMNAttack,
+        atks.L1FMNAttack,
+        atks.L2FMNAttack,
+        atks.LInfFMNAttack,
+        atks.PointwiseAttack,
+
+        atks.FGM,
+        atks.FGSM,
+        atks.L2PGD,
+        atks.LinfPGD,
+        atks.PGD
+
     ]
-    raw_advs, clipped_advs, success = attack(fmodel, images, labels, epsilons=epsilons)
-
-    # calculate and report the robust accuracy (the accuracy of the model when
-    # it is attacked)
-    robust_accuracy = 1 - success.float32().mean(axis=-1)
-    print("robust accuracy for perturbations with")
-    for eps, acc in zip(epsilons, robust_accuracy):
-        print(f"  Linf norm ≤ {eps:<6}: {acc.item() * 100:4.1f} %")
-
-    # we can also manually check this
-    # we will use the clipped advs instead of the raw advs, otherwise
-    # we would need to check if the perturbation sizes are actually
-    # within the specified epsilon bound
-    print()
-    print("we can also manually check this:")
-    print()
-    print("robust accuracy for perturbations with")
-    for eps, advs_ in zip(epsilons, clipped_advs):
-        acc2 = accuracy(fmodel, advs_, labels)
-        print(f"  Linf norm ≤ {eps:<6}: {acc2 * 100:4.1f} %")
-        print("    perturbation sizes:")
-        perturbation_sizes = (advs_ - images).norms.linf(axis=(1, 2, 3)).numpy()
-        print("    ", str(perturbation_sizes).replace("\n", "\n" + "    "))
-        if acc2 == 0:
-            break
 
 
+    errorList = []
+    for attack in attacks:
+        print(f"current test api: {attack.__str__():s}")
+        try:
+                
+            attack = LinfPGD()
+            epsilons = 0.1
+            raw_advs, clipped_advs, success = attack(fmodel, images, labels, epsilons=epsilons)
+
+            # calculate and report the robust accuracy (the accuracy of the model when
+            # it is attacked)
+            robust_accuracy = 1 - success.float32().mean(axis=-1)
+            print("robust accuracy for perturbations with")
+            
+            print(f"  Linf norm ≤ {eps:<6}: {acc.item() * 100:4.1f} %")
+
+            # we can also manually check this
+            # we will use the clipped advs instead of the raw advs, otherwise
+            # we would need to check if the perturbation sizes are actually
+            # within the specified epsilon bound
+            print("manually chech the robust accuracy for perturbations with")
+
+            acc2 = accuracy(fmodel, advs_, labels)
+            print(f"  Linf norm ≤ {eps:<6}: {acc2 * 100:4.1f} %")
+            print("    perturbation sizes:")
+            perturbation_sizes = (advs_ - images).norms.linf(axis=(1, 2, 3)).numpy()
+            print("    ", str(perturbation_sizes).replace("\n", "\n" + "    "))
+            if acc2 == 0:
+                break
+            print("OK")
+        except Exception as e:
+            print(f"An error Occured:{e} while testing api {attack.__str__()}")
+            errorList.append(attack)
+            continue
+
+
+    if len(errorList) == 0:
+        print("Congratulation! All api had passed the test!")
+    else:
+        print("Sorry to tell you that the following api hasn't pass the test:")
+        for item in errorList:
+            print(item__str__())
+    
 if __name__ == "__main__":
     main()
+    
